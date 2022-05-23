d2b8d036412945e0a9394e078837800cd3411311
Simon Valdez <simovaldez414@gmail.com>
2022-05-22 23:44:33 -0400

    vista editar perfil

--- app/src/main/java/com/umss/awichu/menuLateral/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/umss/awichu/menuLateral/ui/slideshow/SlideshowFragment.kt
@@ -1,25 +1,20 @@
 package com.umss.awichu.menuLateral.ui.slideshow
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.ktx.Firebase
+import com.umss.awichu.R
 import com.umss.awichu.databinding.FragmentSlideshowBinding
 import kotlinx.android.synthetic.main.fragment_slideshow.*
-import kotlinx.coroutines.newFixedThreadPoolContext
 
 class SlideshowFragment : Fragment() {
 
@@ -72,5 +67,12 @@ class SlideshowFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        buttonED.setOnClickListener{
+            findNavController().navigate(R.id.edit)
+        }
+        super.onViewStateRestored(savedInstanceState)
     }
 }
