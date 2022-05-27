e3e7497da15324e64bb6f7b1fcf4190615adbddc
Boris-Zeb <zeballos.rodriguez.boris.vicente@gmail.com>
2022-05-26 20:30:29 -0400

    se agrego inicio de sesion

--- app/src/main/java/com/umss/awichu/presentation/authentification/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/umss/awichu/presentation/authentification/login/presenter/LoginPresenter.kt
@@ -56,6 +56,7 @@ class LoginPresenter(signInInteractor: SignInInteractor): LoginContract.LoginPre
 
     }
 
+
     override fun checkEmptyEmail(email: String): Boolean {
         return email.isEmpty()
     }
